16a9e89ffb42f3e69f5a4fa77b1689bfc1338fd5
Felipe Parra <fparrav@icloud.com>
2025-01-20 07:45:41 -0300

    Actualizar imagen de servicio en compose.yaml y agregar manejo de errores en proxies en main.js

--- main.js
+++ main.js
@@ -55,7 +55,15 @@ if (args) {
     for (let destinationAddress in proxies) {
         for (let sourcePort in proxies[destinationAddress]) {
             logger.info(`PROXY: ${sourcePort} --> ${destinationAddress}:${proxies[destinationAddress][sourcePort]}`);
-            tcpProxy.createProxy(sourcePort, destinationAddress, proxies[destinationAddress][sourcePort]);
+            const proxy = tcpProxy.createProxy(sourcePort, destinationAddress, proxies[destinationAddress][sourcePort]);
+            
+            proxy.on('error', (err) => {
+                logger.error('Proxy error:', err);
+            });
+
+            proxy.on('close', () => {
+                logger.warn(`Connection closed for ${destinationAddress}:${sourcePort}`);
+            });
         }
     }
 
