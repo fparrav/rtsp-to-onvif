22048e4640c58ff951174a28c36e4c179397ce40
Felipe Parra <fparrav@icloud.com>
2025-01-20 16:22:21 -0300

    Agregar manejo de errores en main.js para mejorar la estabilidad y asegurar la continuación del proceso en caso de fallos

--- main.js
+++ main.js
@@ -25,56 +25,61 @@ process.on('unhandledRejection', (reason, promise) => {
 });
 
 if (args) {
-    if (process.env.DEBUG) {
-        logger.setLevel('trace');
-    }
-
-    if (!args.config) {
-        logger.info('Please specifiy a config filename!');
-        return -1;
-    }
-
-    let config = readAndCheckConfig(logger, args.config)
-
-    let proxies = {};
-    for (let onvifConfig of config.onvif) {
-
-        let server = new OnvifServer(logger, onvifConfig);
-
-        if (server.getHostname()) {
-
-            logger.info('');
-            server.startHttpServer();
-            server.startDiscovery();
-            if (process.env.DEBUG)
-                server.enableDebugOutput()
-
-            if (!proxies[onvifConfig.target.hostname])
-                proxies[onvifConfig.target.hostname] = {}
-
-            if (onvifConfig.ports.rtsp && onvifConfig.target.ports.rtsp)
-                proxies[onvifConfig.target.hostname][onvifConfig.ports.rtsp] = onvifConfig.target.ports.rtsp;
-            if (onvifConfig.ports.snapshot && onvifConfig.target.ports.snapshot)
-                proxies[onvifConfig.target.hostname][onvifConfig.ports.snapshot] = onvifConfig.target.ports.snapshot;
-        } else {
-            logger.error(`Failed to find IP address for MAC address ${onvifConfig.mac}`)
-            return -1;
+    try { // Añadido bloque try
+        if (process.env.DEBUG) {
+            logger.setLevel('trace');
         }
-    }
 
-    for (let destinationAddress in proxies) {
-        for (let sourcePort in proxies[destinationAddress]) {
-            logger.info(`PROXY: ${sourcePort} --> ${destinationAddress}:${proxies[destinationAddress][sourcePort]}`);
-            try {
-                tcpProxy.createProxy(sourcePort, destinationAddress, proxies[destinationAddress][sourcePort]);
-                logger.info(`Proxy created for ${sourcePort} --> ${destinationAddress}:${proxies[destinationAddress][sourcePort]}`);
-            } catch (err) {
-                logger.error('Failed to create proxy:', err);
-                // Continuar con el siguiente proxy en lugar de detener el proceso
-                continue;
+        if (!args.config) {
+            logger.info('Please specify a config filename!');
+            process.exit(-1); // Cambiado de return -1 a process.exit(-1)
+        }
+
+        let config = readAndCheckConfig(logger, args.config)
+
+        let proxies = {};
+        for (let onvifConfig of config.onvif) {
+
+            let server = new OnvifServer(logger, onvifConfig);
+
+            if (server.getHostname()) {
+
+                logger.info(`Starting server for ${onvifConfig.name}`);
+                server.startHttpServer();
+                server.startDiscovery();
+                if (process.env.DEBUG)
+                    server.enableDebugOutput()
+
+                if (!proxies[onvifConfig.target.hostname])
+                    proxies[onvifConfig.target.hostname] = {}
+
+                if (onvifConfig.ports.rtsp && onvifConfig.target.ports.rtsp)
+                    proxies[onvifConfig.target.hostname][onvifConfig.ports.rtsp] = onvifConfig.target.ports.rtsp;
+                if (onvifConfig.ports.snapshot && onvifConfig.target.ports.snapshot)
+                    proxies[onvifConfig.target.hostname][onvifConfig.ports.snapshot] = onvifConfig.target.ports.snapshot;
+            } else {
+                logger.error(`Failed to find IP address for MAC address ${onvifConfig.mac}`)
+                process.exit(-1); // Cambiado de return -1 a process.exit(-1)
             }
         }
-    }
 
-    return 0;
+        for (let destinationAddress in proxies) {
+            for (let sourcePort in proxies[destinationAddress]) {
+                logger.info(`PROXY: ${sourcePort} --> ${destinationAddress}:${proxies[destinationAddress][sourcePort]}`);
+                try {
+                    tcpProxy.createProxy(sourcePort, destinationAddress, proxies[destinationAddress][sourcePort]);
+                    logger.info(`Proxy created for ${sourcePort} --> ${destinationAddress}:${proxies[destinationAddress][sourcePort]}`);
+                } catch (err) {
+                    logger.error(`Failed to create proxy for ${sourcePort} --> ${destinationAddress}:${proxies[destinationAddress][sourcePort]}:`, err);
+                    // Continuar con el siguiente proxy en lugar de detener el proceso
+                    continue;
+                }
+            }
+        }
+
+        process.exit(0); // Cambiado de return 0 a process.exit(0)
+    } catch (err) {
+        logger.error('Error en el proceso principal:', err);
+        process.exit(-1);
+    }
 }
