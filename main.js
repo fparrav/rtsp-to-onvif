60c1c67d3bb7864589d0bb753c18d611086040ff
Felipe Parra <fparrav@icloud.com>
2025-01-20 11:01:17 -0300

    Eliminar manejo de eventos en proxies en main.js para simplificar la creación de proxies

--- main.js
+++ main.js
@@ -56,24 +56,8 @@ if (args) {
         for (let sourcePort in proxies[destinationAddress]) {
             logger.info(`PROXY: ${sourcePort} --> ${destinationAddress}:${proxies[destinationAddress][sourcePort]}`);
             try {
-                const proxy = tcpProxy.createProxy(sourcePort, destinationAddress, proxies[destinationAddress][sourcePort]);
+                tcpProxy.createProxy(sourcePort, destinationAddress, proxies[destinationAddress][sourcePort]);
                 logger.info(`Proxy created for ${sourcePort} --> ${destinationAddress}:${proxies[destinationAddress][sourcePort]}`);
-
-                proxy.on('data', (data) => {
-                    logger.trace(`Data received on ${sourcePort}: ${data}`);
-                });
-
-                proxy.on('end', () => {
-                    logger.warn(`Connection ended for ${destinationAddress}:${sourcePort}`);
-                });
-
-                proxy.on('error', (err) => {
-                    logger.error('Proxy error:', err);
-                });
-
-                proxy.on('close', () => {
-                    logger.warn(`Connection closed for ${destinationAddress}:${sourcePort}`);
-                });
             } catch (err) {
                 logger.error('Failed to create proxy:', err);
             }
